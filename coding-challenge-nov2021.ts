0ce393e08a81603b8563aff5ea9c193602ca831d
aywings <30546313+aywings@users.noreply.github.com>
2021-10-26 03:58:05 +0800

    update code blocks

--- coding-challenge-nov2021.ts
+++ coding-challenge-nov2021.ts
@@ -1,53 +1,33 @@
-// enums
-enum Direction {
-    //% block="forward"
-    Forward,
-    //% block="back"
-    Back,
-    //% block="left"
-    Left,
-    //% block="right"
-    Right,
-    //% block="up"
-    Up,
-    //% block="down"
-    Down
-}
-
 // global variables
 const bookshelf = 47;
 const laundry = 724;
 
-const directions = [
-    FORWARD,
-    BACK,
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-];
-
-//% block="LPC" weight=200 color=#3296BC icon="\uf57e"
+//% block="LPC" weight=200 color=#3296BC icon="\uf0c9"
 namespace lpc {
     /**
      * Pick up books in the d direction
      */
     //% block="Pick up books %d"
-    export function pickupBooks(d: Direction): void {
-        const direction = directions[d];
+    export function pickupBooks(direction: SixDirection): void {
         if (agent.inspect(AgentInspection.Block, direction) == bookshelf) {
             agent.destroy(direction);
             const count = agent.getItemCount(1);
             agent.setItem(bookshelf, count + 1, 1)
         }
     }
+    
+    /**
+     * Detect whether there are books in the d direction
+     */
+    //% block="Detect books %d"
+    export function detectBooks(direction: SixDirection): boolean {
+        return agent.inspect(AgentInspection.Block, direction) == bookshelf
 
     /**
      * Place books in the d direction
      */
     //% block="Place down books %d"
-    export function placeBooks(d: Direction): void {
-        const direction = directions[d];
+    export function placeBooks(direction: SixDirection): void {
         if (agent.getItemCount(1) > 0 && agent.getItemDetail(1) == bookshelf) {
             agent.place(direction);
         }
@@ -58,16 +38,25 @@ namespace lpc {
      */
     //% block="Agent walks upstairs"
     export function walkUpstairs() {
-        agent.move(directions[Direction.Up], 1);
-        agent.move(directions[Direction.Down], 1);
+        agent.move(UP, 1);
+        agent.move(FORWARD, 1);
     }
+    
+    /**
+     * Agent walks downstairs
+     */
+    //% block="Agent walks downstairs"
+    export function walkUpstairs() {
+        agent.move(FORWARD, 1);
+        agent.move(DOWN, 1);
+    }
+
 
     /**
      * Pick up laundry in the d direction
      */
     //% block="Pick up laundry %d"
-    export function pickupLaundry(d: Direction): void {
-        const direction = directions[d];
+    export function pickupLaundry(direction: SixDirection): void {
         if (agent.inspect(AgentInspection.Block, direction) == laundry) {
             agent.destroy(direction);
             const count = agent.getItemCount(2);
@@ -79,10 +68,16 @@ namespace lpc {
      * Place laundry in the d direction
      */
     //% block="Place down laundry %d"
-    export function placeLaundry(d: Direction): void {
-        const direction = directions[d];
+    export function placeLaundry(direction: SixDirection): void {
         if (agent.getItemCount(2) > 0 && agent.getItemDetail(2) == laundry) {
             agent.place(direction);
         }
     }
+        
+    /**
+     * Detect whether there are laundry in the d direction
+     */
+    //% block="Detect laundry %d"
+    export function detectBooks(direction: SixDirection): boolean {
+        return agent.inspect(AgentInspection.Block, direction) == laundry
 }
